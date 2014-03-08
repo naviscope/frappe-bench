efb00e3689b7b8b98fb2750dba924f0f697d1cb8
Pratik Vyas <m@pd.io>
2014-03-09 00:35:04 +0530

    print output on command failure

--- scripts/virtualenv.py
+++ scripts/virtualenv.py
@@ -2345,15 +2345,20 @@ def after_install(options, home_dir):
         apps = json.load(f)
     app_names = [app['name'] for app in apps]
     os.chdir('apps')
-    # subprocess.check_output('/'.join([home_dir, 'bin', 'pip install --no-index --find-links=../virtualenv_support lxml']), shell=True)
-    if not os.path.exists('frappe'):
-        subprocess.check_output('git clone http://github.com/frappe/frappe/ --origin upstream', shell=True)
-    subprocess.check_output('/'.join([home_dir, 'bin', 'pip install -e frappe/ ']), shell=True)
+    try:
+        # subprocess.check_output('/'.join([home_dir, 'bin', 'pip install --no-index --find-links=../virtualenv_support lxml']), shell=True)
+        if not os.path.exists('frappe'):
+            subprocess.check_output('git clone https://github.com/frappe/frappe.git --origin upstream', shell=True)
+        subprocess.check_output('/'.join([home_dir, 'bin', 'pip install -e frappe/ ']), shell=True)
 
-    for app in apps:
-        if not os.path.exists(app['name']):
-            subprocess.check_output('git clone {app_url} {app_name} --origin upstream'.format(app_url=app['url'], app_name=app['name']), shell=True)
-        subprocess.check_output('/'.join([home_dir, 'bin', 'pip install -e {}/'.format(app['name'])]), shell=True)
+        for app in apps:
+            if not os.path.exists(app['name']):
+                subprocess.check_output('git clone {app_url} {app_name} --origin upstream'.format(app_url=app['url'], app_name=app['name']), shell=True)
+            subprocess.check_output('/'.join([home_dir, 'bin', 'pip install -e {}/'.format(app['name'])]), shell=True)
+    except subprocess.CalledProcessError, e:
+        print "Error:", e.output
+        raise
+
     os.chdir('..')
     with open('sites/apps.txt', 'w') as f:
         f.write('\n'.join(app_names))
